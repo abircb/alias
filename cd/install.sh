d7f050bd4d60a9b42c1e8d68271e7f33c68cd884
abir <abirchandrabhushan@gmail.com>
2020-04-24 15:21:11 +0530

    add gulp and cd

---
+++ cd/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# change directories easily
+alias ..='cd ../'
+echo "added .. : an alias for cd ../"
+
+alias ...='cd ../../'
+echo "added ... : an alias for cd ../../"
+
+alias ....='cd ../../..'
+echo "added .... : an alias for cd ../../../"
+
+alias .....='cd ../../../..'
+echo "added ..... : an alias for cd ../../../../"
---
+++ gulp/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Gulp common
+alias gus='gulp serve'
+echo 'added gus: an alias for gulp serve'
+
+alias gub='gulp build'
+echo 'added gub: an alias for gulp build'
+
+# Gulp test
+alias gut='gulp test'
+echo 'added gut: an alias for gulp test'
