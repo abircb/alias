b6e5c9e1d82d4113d64501246d2823dc28f805ab
abir <abirchandrabhushan@gmail.com>
2020-04-24 15:07:07 +0530

    add more aliases

---
+++ npm/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Npm install alias
+alias npi='npm install'
+echo 'added npi: an alias for npm install'
+
+alias npis='npm install --save'
+echo 'added npis: an alias for npm install --save'
+
+# Npm update
+alias npu='npm update'
+echo 'added npu: an alias for npm update'
+
+# Npm search
+alias nps='npm search'
+echo 'added nps: an alias for npm search'
---
+++ youtube/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# alias for youtube
+echo "alias youtube='open https://youtube.com'" >>  ~/.bash_profile ; . ~/.bash_profile
+echo 'added youtube: an alias for opening Youtube in your browser'
