21cad0cfa6511c7cb22bd8e47c27c02cbeeb703a
abir <abirchandrabhushan@gmail.com>
2020-07-22 22:26:15 +0530

    add yarn

---
+++ yarn/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# yarn add alias
+echo 'added yadd: an alias for yarn add'
+alias yadd='yarn add'
+
+# yarn add --save alias
+echo 'added yadev: an alias for yarn add --dev'
+alias yadev='yarn add --dev'
+
+# yarn build alias
+echo 'added yab: an alias for yarn build'
+alias yab='yarn build'
+
+# yarn serve alias
+echo 'added yas: an alias for yarn run serve'
+alias yas='yarn run serve'
+
+
